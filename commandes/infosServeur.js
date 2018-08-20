792e3421c366d18a62031387161435cc244fe61b
O-YamaBullShit <42041037+O-YamaBullShit@users.noreply.github.com>
2018-08-20 14:21:33 +0200

    Update infosServeur.js

--- commandes/infosServeur.js
+++ commandes/infosServeur.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   let servericon = message.guild.iconURL;
+  let afkChan = message.guild.afkChannel
+  if(afkChan == null){
+    afkChan = "Non défini"
+  }
   let serverembed = new Discord.RichEmbed()
   .setDescription("Infos du serveur")
   .setColor('#a5b1ff')
@@ -10,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
   .addField('Serveur crée le :', message.guild.createdAt)
   .addField('Le proprietaire est :', message.guild.owner)
   .addField('Vous avez rejoint le :', message.member.joinedAt)
+  .addField("Le canal AFK est :", afkChan)
   .addField('Nombres de membres au total :', message.guild.memberCount);
   return message.channel.send(serverembed);
 }
