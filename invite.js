d617da16bb406a75508b362a3ce1a924778a5e9f
O-YamaBullShit <42041037+O-YamaBullShit@users.noreply.github.com>
2018-10-07 18:54:41 +0200

    Add files via upload

---
+++ invite.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+	message.channel.send("https://bit.ly/2NuRJRs");
+}
+module.exports.help = {
+	name: "invite"
+}
