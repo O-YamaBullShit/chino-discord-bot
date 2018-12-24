aa6909c11ae43d8ce2c04e407ab35563578bfa07
Théo Champion <42041037+O-YamaBullShit@users.noreply.github.com>
2018-12-24 23:55:12 +0100

    Add files via upload

---
+++ commandes/noel.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+	message.channel.send("Je vous souhaite un joyeux noël à tous mes chéris !")
+}
+module.exports.help = {
+	name: "christmas"
+}
