5e99051adb19448f830c5091c54f2e1673dfec6b
Théo Champion <42041037+O-YamaBullShit@users.noreply.github.com>
2018-12-26 22:16:31 +0100

    Update help.js

--- commandes/help.js
+++ commandes/help.js
@@ -8,7 +8,8 @@ module.exports.run = async (bot, message, args) => {
       .setThumbnail(boticon)
       .addField("Commandes utiles", "`§infosBot` `§infosServeur` `§invite` `§pileOuFace` `§help` `§ping` `§proposition [commande] [effet de la commande]` `§signaler [mention] [raison du signalement]` (⚠ Vous pouvez vous prendre un avertissement si vous signalez a tort ⚠)")
       .addField("Commandes reservées a la modération", "`§kick [mention] [raison du kick]` `§ban [mention] [raison du ban]` `§warn [mention] [raison du warn]` (au bout de 3 warns l'utilisateur averti est automatiquement banni) `§mute [mention] [temps du mute (ex : 10m pour 10 minutes)]` `§unmute [utilisateur mute]` `§clear [nombre de messages a supprimer] (100 max)`")
-      .addField("Commande relative aux goldous", "`§coins` `§pay [mention] [nbr de goldous a payer]` `§coinsgive [mention] [nbr de goldous a donner]` (Attention la dernière commande est __**UNIQUEMENT**__ réalisable par des admins)");
+      .addField("Commande relative aux goldous", "`§coins` `§pay [mention] [nbr de goldous a payer]` `§coinsgive [mention] [nbr de goldous a donner]` (Attention la dernière commande est __**UNIQUEMENT**__ réalisable par des admins)")
+      .addField("Commandes autres", "`§noel` `§8ball` `§nsfw [tags (5 max)]`");
     message.channel.send(helpEmbed);
 }
 
