1a376b65064fc1b13a213b91c11141e578a6c25e
Théo Champion <42041037+O-YamaBullShit@users.noreply.github.com>
2018-12-26 20:25:46 +0100

    Update nsfw_test.js

--- commandes/nsfw_test.js
+++ commandes/nsfw_test.js
@@ -1,15 +1,11 @@
-const Discord = require('discord.js');
+const discord = require("discord.js");
+const Kaori = require("kaori");
+const kaori = new Kaori();
 
-module.exports.run = async (bot, message, args) => {
-  let nsfVerify = "nsfw-"
-  if(TextChannel.name.includes(nsfVerify)){
-    message.reply("Channel NSFW !")
-  }
-  else{
-    message.reply("Channel non NSFW")
-  }
+module.exports.run = async(bot, message, args) =>{
+	if(message.channel.nsfw != true) return message.reply("Non ! Je n'enverrai pas ce genre d'images dans un channel non-NSFW inutile d'insister !")
+	kaori.search('gelbooru', {tags: [args[0], args[1], args[2], args[3], args[4]], limit: 1, random: true}).then(images => message.channel.send(images[0].common.fileURL)).catch(err => console.log(err));
 }
-
 module.exports.help = {
-  name: "nsfwTest"
+	name: "nsfw"
 }
